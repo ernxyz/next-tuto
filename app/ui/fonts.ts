9a7c9de253aa9ddfe6341b05a55ce020a46a1b85
ernxyz <ernej29@gmail.com>
2025-11-12 21:52:44 -0500

    font fix

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,5 +1,5 @@
 import { Lusitana } from "next/font/google";
 import { Noto_Sans } from "next/font/google";
 
-export const lusitana = Lusitana({ weight: ["400"] });
+export const lusitana = Lusitana({ weight: ["400"], subsets: ["latin"] });
 export const notoSans = Noto_Sans({ subsets: ["latin"] });
